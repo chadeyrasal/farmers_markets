ac8c223ae4e69c1d2842fddddc6a8bd9657ca917
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-21 17:01:48 +0000

    Modification of controller class call method

--- lib/farmers_markets/controller.rb
+++ lib/farmers_markets/controller.rb
@@ -1,31 +1,36 @@
 class FarmersMarkets::Controller
   
   def call
-    puts " "
     puts "Welcome Local Food Hunters!"
-    puts " "
-    puts "  To see your options, type in 'menu'"
-    puts "  To leave the application, type in 'exit'"
-    puts "  What would you like to do?"
-    puts " "
-    user_input = gets.strip
-    if user_input.upcase == "MENU"
-      menu
-    else
-      puts " "
-      puts "  What would you like to do?"  
-      puts " "
-      user_input = gets.strip
+    main_prompt
+    user_input = gets.strip.downcase
+    case user_input
+    when "menu"
+      display_menu
+    when "list markets"
+      list_markets
+    when "list districts"
+      list_districts
+    when "list districts with markets"
+      list_districts_with_markets
+    when "list markets in district"
+      list_markets_in_district
+    when "market"
+      market_details
     end
-    until user_input.upcase == "EXIT"
-      puts " "
-      puts "  What would you like to do?"  
-      puts " "
-      user_input = gets.strip
+    until user_input == "exit"
+      main_prompt
+      user_input = gets.strip.downcase
     end
   end
   
-  def menu
+  def main_prompt
+    puts "To see your options, type in 'menu'"
+    puts "To leave the application, type in 'exit'"
+    puts "What would you like to do?"  
+  end
+  
+  def display_menu
     puts " "
     puts "To list all Farmers Markets in the UK, enter 'list markets'"
     puts "To list all UK districts, enter 'list districts'"
@@ -33,18 +38,6 @@ class FarmersMarkets::Controller
     puts "To list a district's Farmers Markets, enter 'list markets in the district'"
     puts "To have the details of a specific market, enter 'market'"
     puts " "
-    user_input = gets.strip
-    if user_input.upcase == "LIST MARKETS"
-      list_markets
-    elsif user_input.upcase == "LIST DISTRICTS"
-      list_districts
-    elsif user_input.upcase == "LIST DISTRICTS WITH MARKETS"
-      list_districts_with_markets
-    elsif user_input.upcase == "LIST MARKETS IN THE DISTRICT"
-      list_markets_in_the_district
-    elsif user_input.upcase == "MARKET"
-      market_details
-    end
   end
   
   def list_markets
@@ -53,15 +46,25 @@ class FarmersMarkets::Controller
   end
   
   def list_districts
+    puts "District 1 - Dundee - DD"
+    puts "District 2 - Bath - BA"
   end
   
   def list_districts_with_markets
+    puts "District 1 - Rochester - ME"
   end
   
-  def list_markets_in_the_district
+  def list_markets_in_district
+    puts "Market 1 - East Farleigh Market"
+    puts "Market 2 - Elm Court Village Market"
   end
   
   def market_details
+    puts "Name: East Farleigh Market"
+    puts "Location: ME15 0JL"
+    puts "Phone Number: 01732833976"
+    puts "Email: bob@kfma.org.uk"
+    puts "Website: https://www.kfma.org.uk"
   end
   
 end
--- lib/farmers_markets/market.rb
+++ lib/farmers_markets/market.rb
@@ -1,2 +1,3 @@
 class FarmersMarkets::Market
+  
 end
