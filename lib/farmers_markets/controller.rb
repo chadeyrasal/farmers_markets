e329bd2ca6e2e4e13e19a5312f2906e0eede8e15
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-21 10:26:06 +0000

    Modification of controller class

--- lib/farmers_markets/controller.rb
+++ lib/farmers_markets/controller.rb
@@ -1,27 +1,38 @@
 class FarmersMarkets::Controller
   
   def call
-    puts "Welcome Local Food Hunters"
-    puts "To see your options, type in 'menu'"
-    puts "To leave the application, type in 'exit'"
-    puts "What would you like to do?"
+    puts " "
+    puts "Welcome Local Food Hunters!"
+    puts " "
+    puts "  To see your options, type in 'menu'"
+    puts "  To leave the application, type in 'exit'"
+    puts "  What would you like to do?"
+    puts " "
     user_input = gets.strip
     if user_input.upcase == "MENU"
       menu
-    elsif user_input.upcase == "EXIT"
-      exit
     else
-      puts "What would you like to do?"  
+      puts " "
+      puts "  What would you like to do?"  
+      puts " "
+      user_input = gets.strip
+    end
+    until user_input.upcase == "EXIT"
+      puts " "
+      puts "  What would you like to do?"  
+      puts " "
       user_input = gets.strip
     end
   end
   
   def menu
+    puts " "
     puts "To list all Farmers Markets in the UK, enter 'list markets'"
     puts "To list all UK districts, enter 'list districts'"
     puts "To list all districts that have at least one Farmers Market, enter 'list districts with markets'"
     puts "To list a district's Farmers Markets, enter 'list markets in the district'"
     puts "To have the details of a specific market, enter 'market'"
+    puts " "
     user_input = gets.strip
     if user_input.upcase == "LIST MARKETS"
       list_markets
@@ -37,6 +48,8 @@ class FarmersMarkets::Controller
   end
   
   def list_markets
+    puts "Market 1 - London Fields - E8"
+    puts "Market 2 - Islington Town Hall - N1"
   end
   
   def list_districts
