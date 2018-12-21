c70034ed621a391e93179cf61b044ae2153f3845
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-21 17:15:35 +0000

    modification of market class and district class

--- lib/farmers_markets/district.rb
+++ lib/farmers_markets/district.rb
@@ -2,5 +2,6 @@ class FarmersMarkets::District
 
 # a district has a name
 # a district has a code
+# a district
   
 end
--- lib/farmers_markets/market.rb
+++ lib/farmers_markets/market.rb
@@ -1,3 +1,10 @@
 class FarmersMarkets::Market
+
+# a market has a name
+# a market has a location or postcode
+# a market has a district
+# a market has a phone number
+# a market has a email
+# a market has a website
   
 end
