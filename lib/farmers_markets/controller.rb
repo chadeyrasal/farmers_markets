42585043b7e29e60e704447a0e4b6629dbc207f3
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-24 09:00:02 +0000

    Modification of call method in controller and modification of menu structure

--- lib/farmers_markets/controller.rb
+++ lib/farmers_markets/controller.rb
@@ -2,71 +2,64 @@ class FarmersMarkets::Controller
   
   def call
     puts "Welcome Local Food Hunter!"
-    puts "What would you like to do?"
-    user_input = gets.strip.downcase
-    case user_input
-    when "menu"
-      display_menu
-    when "list markets"
-      list_markets
-    when "list districts"
-      list_districts
-    when "list districts with markets"
-      list_districts_with_markets
-    when "list markets in district"
-      list_markets_in_district
-    when "market"
-      market_details
-    when "exit"
-      goodbye
-      exit
-    else
-      puts "I am not sure I understand what you want"
-    end
-    while user_input != "exit"
-      puts "What would you like to do now?"
-      user_input = gets.strip.downcase
-    end
-    goodbye
-  end
-  
-  def call
-    puts "Welcome Local Food Hunter!"
     menu
     goodbye
   end
   
   def menu
-    puts "To list all Farmers Markets in the UK, enter 'list markets'"
-    puts "To list all UK districts, enter 'list districts'"
-    puts "To list all districts that have at least one Farmers Market, enter 'list districts with markets'"
-    puts "To list a district's Farmers Markets, enter 'list markets in the district'"
+    space
+    puts "To list districts that have farmers markets, enter 'ld'"
+    puts "To list all farmers markets in a specific district, enter 'lm'"
     puts "To have the details of a specific market, enter 'market'"
-    puts " "
+    puts "To leave the application, enter 'exit'"
+    space
+    user_input = nil
+    while user_input != "exit"
+      space
+      puts "What would you like to do?"
+      space
+      user_input = gets.strip.downcase
+      case user_input
+      when "ld"
+        list_districts
+      when "lm"
+        list_markets
+      when "market"
+        market_details
+      when "exit"
+        goodbye
+        exit
+      else
+        space
+        puts "I am not sure I understand what you want"
+        puts "What would you like to do?"
+        space
+      end
+    end
+    
   end
   
   def goodbye
+    space
     puts "Goodbye, see you for the next market!"
   end
   
+  def space
+    puts " "
+  end
+  
   def list_markets
+    space
     FarmersMarkets::Market.all
   end
   
   def list_districts
+    space
     FarmersMarkets::District.all
   end
   
-  def list_districts_with_markets
-    puts "District 1 - Rochester - ME"
-  end
-  
-  def list_markets_in_district
-    puts "Market 1 - East Farleigh Market"
-    puts "Market 2 - Elm Court Village Market"
-  end
-  
   def market_details
+    space
     puts "Name: East Farleigh Market"
     puts "Location: ME15 0JL"
     puts "Phone Number: 01732833976"
