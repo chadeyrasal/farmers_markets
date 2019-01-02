8d44bff1c58a38832423b3f19ae11b1e8cf55d14
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-02 21:03:30 +0000

    modif of scraper class

--- lib/farmers_markets/market.rb
+++ lib/farmers_markets/market.rb
@@ -1,6 +1,6 @@
 class FarmersMarkets::Market
   
-  attr_accessor :name, :postcode, :district, :phone_number, :email, :website
+  attr_accessor :name, :postcode, :district, :phone_number, :email, :website, :membership
   
   @@all = []
   
@@ -16,17 +16,6 @@ class FarmersMarkets::Market
     [market1, market2]
   end
   
-  def self.scrape_markets
-    doc = Nokogiri::HTML(open("http://www.farma.org.uk/members-map/"))
-    my_markets = []
-    doc.css("div.map-member").each do |member|
-      this_market = {}
-      this_market[:name] = member.css("h4").text
-      this_market[:postcode] = member.css("div.postcode").text
-      this_market[:details_link] = member.css("a.read-more").attribute("href").value
-      my_markets << this_market
-    end
-    my_markets
-  end
+
   
 end
--- lib/farmers_markets/scraper.rb
+++ lib/farmers_markets/scraper.rb
@@ -4,9 +4,26 @@ require 'pry'
 
 class Scraper
   
-  def self.scrape_districts(url_uk_postcode)
-    html = File.read(url_uk_postcode)
-    all_districts = Nokogiri::HTML(html)
+  def self.scrape_markets
+    doc = Nokogiri::HTML(open("http://www.farma.org.uk/members-map/"))
+    my_markets = []
+    doc.css("div.map-member").each do |member|
+      this_market = {}
+      this_market[:name] = member.css("h4").text
+      this_market[:postcode] = member.css("div.postcode").text
+      this_market[:details_link] = member.css("a.read-more").attribute("href").value
+      my_markets << this_market
+    end
+    my_markets
+  end
+  
+  def self.scrape_contact_details(market_url)
+    doc = Nokogiri::HTML(open(market_url))
+    market_details = []
+    doc.css("").each do |detail|
+      contact_details = {}
+      contact_details[:address] = detail.css("")
+    end
   end
   
 end
